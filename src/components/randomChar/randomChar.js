541dc70d296c62bd8c4e1db7e0fe779bce4134d0
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-07-28 14:03:12 +0300

    clone project & refactor components style

---
+++ src/components/randomChar/randomChar.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const RandomCharBlock = styled.div`
+    background-color: #fff;
+    padding: 25px 25px 15px 25px;
+    margin-bottom: 40px;
+`
+
+const RandomCharHeader = styled.h4`
+    margin-bottom: 20px;
+    text-align: center;
+`
+
+export default class RandomChar extends Component {
+
+    render() {
+
+        return (
+            <RandomCharBlock className="rounded">
+                <RandomCharHeader>Random Character: John</RandomCharHeader>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Gender </span>
+                        <span>male</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Born </span>
+                        <span>11.03.1039</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Died </span>
+                        <span>13.09.1089</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between">
+                        <span className="term">Culture </span>
+                        <span>Anarchy</span>
+                    </li>
+                </ul>
+            </RandomCharBlock>
+        );
+    }
+}
