57cb82da6cfec87ff456cc1d0a773594cfb31686
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-07-28 18:48:45 +0300

    randomChar component

---
+++ src/components/errorMessage/errorMessage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+import img from './error.jpg'
+
+const ImgError = styled.img`
+    width: 100%
+`
+
+const ErrorMessage = () => {
+    return (
+        <>
+            <ImgError src={img} alt="error"/>
+            <span>Something goes wrong...</span>
+        </>
+    )
+}
+
+export default ErrorMessage;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-vjw07k8cfs"><div className="ldio-g5sr4rokea">
+        <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
