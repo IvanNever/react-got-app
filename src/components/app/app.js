c8f20f751c5659b0eaa09678b6838155a08afc48
Ivan Neverovsky <ivan@neverovsky.com.ua>
2020-07-31 13:56:19 +0300

    navigation with React Routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,10 +4,10 @@ import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorMessage from '../error';
 import {CharacterPage, BooksPage, HousesPage, BooksItem} from '../pages';
-import ItemList from '../itemList';
-import ItemDetails from '../itemDetails';
 import gotService from '../../services/gotService';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
+import './app.css';
 
 export default class App extends Component {
 
@@ -40,31 +40,39 @@ export default class App extends Component {
         }
 
         return (
-            <>
-                <Container>
-                    <Header />
-                </Container>
-                <Container>
-                    <Row>
-                        <Col lg={{size: 5, offset: 0}}>
-                            {char}
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md='3'>
-                            <Button
-                                color='primary'
-                                className='mb-5'
-                                onClick={this.toggleRandomChar}>
-                                    Toggle Random Character
-                            </Button>
-                        </Col>
-                    </Row>
-                    <CharacterPage/>
-                    <BooksPage/>
-                    <HousesPage/>
-                </Container>
-            </>
+            <Router>
+                <div className="app">
+                    <Container>
+                        <Header />
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col lg={{size: 5, offset: 0}}>
+                                {char}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md='3'>
+                                <Button
+                                    color='primary'
+                                    className='mb-5'
+                                    onClick={this.toggleRandomChar}>
+                                        Toggle Random Character
+                                </Button>
+                            </Col>
+                        </Row>
+                        <Route path='/characters' component={CharacterPage}/>
+                        <Route path='/books' exact component={BooksPage}/>
+                        <Route path='/houses' component={HousesPage}/>
+                        <Route path='/books/:id' render={
+                            ({match}) => {
+                                const {id} = match.params;
+                                return <BooksItem bookId={id}/>
+                            }
+                        }/>
+                    </Container>
+                </div>
+            </Router>
         );
     }
 
